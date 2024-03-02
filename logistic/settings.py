54fd7659af44e66cba7bba142f7363d652522a88
ptoski716 <patoski716@gmail.com>
2024-03-02 18:45:33 +0100

    adding cors orign

--- logistic/settings.py
+++ logistic/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'contact',
     'tracking',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:5173',
+    # Add other origins as needed
+]
