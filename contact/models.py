5dc8829611c237edb62387a9947f261dd5cec225
ptoski716 <patoski716@gmail.com>
2024-03-02 20:18:36 +0100

    adding cors orign

--- contact/models.py
+++ contact/models.py
@@ -8,7 +8,7 @@ class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     subject = models.CharField(max_length=100)
-    phone = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100,null=True,blank=True,default="")
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
